0e29d530887322678a75435043c1a7b22b6d1e64
aetrnm <oleggnatkovskii@gmail.com>
2021-01-25 16:44:02 +0200

    field background and resource created

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -4,6 +4,13 @@
 GameWindow::GameWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+
+    QPixmap bkgnd(":/gamecomponents/images/field.png");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bkgnd);
+    this->setPalette(palette);
+    this->setFixedSize(QSize(479, 656));
 }
 
 GameWindow::~GameWindow()
